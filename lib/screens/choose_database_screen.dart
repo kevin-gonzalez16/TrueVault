9beeb7b2daacd0486a7cf243414a5474a6d33328
alfonso <a.barrera.29123@gmail.com>
2022-09-19 18:42:57 -0500

    Refactored and added tests to verify the cancel buttons and the template functions

--- lib/screens/choose_database_screen.dart
+++ lib/screens/choose_database_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:true_vault/screens/main_screen.dart';
+
 
 class ChooseDatabase extends StatefulWidget {
   const ChooseDatabase({Key? key}) : super(key: key);
@@ -6,51 +8,62 @@ class ChooseDatabase extends StatefulWidget {
   @override
   State<ChooseDatabase> createState() => _ChooseDatabaseState();
 }
+
 //Mock Database for now while we make data dynamic
 List databases = ["Database 1","Database 2","Database 3","Database 4","Database 5","Database 6","Database 7"];
 
 //Template to list over the databases
 Widget chooseDatabaseTemplate(database){
+  //Individual Database Buttons
   return Padding(
-    padding: const EdgeInsets.fromLTRB(8, 8, 8, 10),
-    child:
-    TextButton(
-        onPressed: ()=>{},
-        style: TextButton.styleFrom(
-          backgroundColor: const Color.fromRGBO(24, 154, 180, 1),
-          minimumSize: const Size(150,75),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        ),
-        child:  Text(database,style: const TextStyle(color: Colors.white, fontSize: 20),)
+    padding: const EdgeInsets.fromLTRB(8, 3, 8, 10),
+    child: AspectRatio(
+      aspectRatio: 16/4,
+      child: TextButton(
+          onPressed: ()=>{
+          },
+          style: TextButton.styleFrom(
+            backgroundColor: const Color.fromRGBO(24, 154, 180, 1),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          ),
+          child: Text(database,style: const TextStyle(color: Colors.white, fontSize: 20),)
+      ),
     ),
   );
 }
 
 class _ChooseDatabaseState extends State<ChooseDatabase> {
+
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: const Color.fromRGBO(23, 42, 58, 1.0),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(
-              height: 625,
-              width: 325,
-              child: Container(
-                  decoration: const BoxDecoration(
-                    color: Color.fromRGBO(239, 239, 239, 1.0),
-                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                  ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: SizedBox(
+                height: height-75,
+                width: width-75,
+                child: Container(
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       Row(
                         mainAxisAlignment: MainAxisAlignment.start,
                         children: [
                           TextButton(
-                            onPressed: ()=>Navigator.pushReplacementNamed(context, 'main_screen'),
+                            key: const Key("chooseDatabaseCancelButton"),
+                            onPressed: (){
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const MainScreen()),
+                              );
+                            },
                             style: TextButton.styleFrom(
                               primary: const Color.fromRGBO(165, 165, 165, 1),
                             ),
@@ -61,10 +74,9 @@ class _ChooseDatabaseState extends State<ChooseDatabase> {
                         ],
                       ),
                       Padding(
-                        padding: const EdgeInsets.fromLTRB(30, 5, 30, 25),
-                        child: SizedBox(
-                          height: 100,
-                          width: 275,
+                        padding: const EdgeInsets.fromLTRB(25,25,25,5),
+                        child: AspectRatio(
+                          aspectRatio: 15/5,
                           child: Container(
                             decoration: const BoxDecoration(
                               color:Color.fromRGBO(95, 116, 170, 1),
@@ -79,19 +91,29 @@ class _ChooseDatabaseState extends State<ChooseDatabase> {
                           ),
                         ),
                       ),
-                      SizedBox(
-                        height: 400,
-                        child:ListView(children: databases.map((database) => chooseDatabaseTemplate(database)).toList(),),
+                      Expanded(
+                          child: SizedBox(
+                            child: ListView.builder(
+                              scrollDirection: Axis.vertical,
+                              itemCount: databases.length,
+                              itemBuilder: (BuildContext test, int index){
+                                return chooseDatabaseTemplate(databases[index]);
+                              },
+                            )
+                          )
                       ),
-
                     ],
+                  ),
+                  decoration: const BoxDecoration(
+                    color: Color.fromRGBO(239, 239, 239, 1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
                   )
+                ),
               ),
-            ),
-
+            )
           ],
         ),
-      ),
+      )
 
     );
   }
--- lib/screens/delete_database_screen.dart
+++ lib/screens/delete_database_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:true_vault/screens/main_screen.dart';
 
 class DeleteDatabase extends StatefulWidget {
   const DeleteDatabase({Key? key}) : super(key: key);
@@ -13,9 +14,9 @@ List databases = ["Database 1","Database 2","Database 3","Database 4","Database 
 //Template to list over the database
 Widget deleteDatabaseTemplate(database){
   return Padding(
-    padding: const EdgeInsets.fromLTRB(0, 0, 8, 20),
+    padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
     child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisAlignment: MainAxisAlignment.start,
       children: [
         ElevatedButton(onPressed: ()=>{},
             style: ElevatedButton.styleFrom(
@@ -28,11 +29,12 @@ Widget deleteDatabaseTemplate(database){
             onPressed: ()=>{},
             style: TextButton.styleFrom(
               backgroundColor: const Color.fromRGBO(24, 154, 180, 1),
-              minimumSize: const Size(250,50),
+              minimumSize: const Size(240,50),
               shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
             ),
             child:  Text(database,style: const TextStyle(color: Colors.white, fontSize: 25),)
         ),
+
       ],
     ),
   );
@@ -41,65 +43,81 @@ Widget deleteDatabaseTemplate(database){
 class _DeleteDatabaseState extends State<DeleteDatabase> {
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: const Color.fromRGBO(23, 42, 58, 1.0),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            SizedBox(
-              height: 625,
-              width: 325,
-              child: Container(
-                  decoration: const BoxDecoration(
-                    color: Color.fromRGBO(239, 239, 239, 1.0),
-                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                  ),
-                  child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+        Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SizedBox(
+          height: height-75,
+          width: width-75,
+          child: Container(
+              child: Column(
+                children: [
+                  Row(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          TextButton(
-                            onPressed: ()=>{Navigator.pushReplacementNamed(context, "main_screen")},
-                            style: TextButton.styleFrom(
-                              primary: const Color.fromRGBO(165, 165, 165, 1),
-                            ),
-                            child: const Text("cancel",
-                              style: TextStyle(fontSize: 15),
-                            ),
-                          ),
-                        ],
+                      TextButton(
+                        key: const Key("deleteDatabaseCancelButton"),
+                        onPressed: ()=>{
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const MainScreen()),
+                          )
+                        },
+                        style: TextButton.styleFrom(
+                          primary: const Color.fromRGBO(165, 165, 165, 1),
+                        ),
+                        child: const Text("cancel",
+                          style: TextStyle(fontSize: 15),
+                        ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(30, 5, 30, 25),
-                        child: SizedBox(
-                          height: 100,
-                          width: 275,
-                          child: Container(
-                            decoration: const BoxDecoration(
-                              color:Color.fromRGBO(255, 94, 94, 1),
-                              borderRadius: BorderRadius.all(Radius.circular(15)),
-                            ),
-                            child: const Center(
-                              child: Text("WARNING!\nDeleting the database will permanently remove all the\nrecords!",
-                                style: TextStyle(fontSize: 20,color:Colors.white),
-                                textAlign: TextAlign.center,
-                              ),
-                            ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(25,25,25,5),
+                    child: AspectRatio(
+                      aspectRatio: 15/6,
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          color:Color.fromRGBO(255, 94, 94, 1),
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                        ),
+                        child: const Center(
+                          child: Text("WARNING!\nDeleting the database will permanently remove all the\nrecords!",
+                            style: TextStyle(fontSize: 20,color:Colors.white),
+                            textAlign: TextAlign.center,
                           ),
                         ),
                       ),
-                      SizedBox(
-                        height: 400,
-                        child:ListView(children: databases.map((database) => deleteDatabaseTemplate(database)).toList(),),
-                      ),
+                    ),
+                  ),
+                  Expanded(
+                      child: SizedBox(
+                          child: ListView.builder(
+                            scrollDirection: Axis.vertical,
+                            itemCount: databases.length,
+                            itemBuilder: (BuildContext test, int index){
+                              return deleteDatabaseTemplate(databases[index]);
+                            },
+                          )
+                      )
+                  ),
 
                     ],
-                  )
+                  ),
+              decoration: const BoxDecoration(
+                color: Color.fromRGBO(239, 239, 239, 1.0),
+                borderRadius: BorderRadius.all(Radius.circular(15.0)),
+              )
               ),
-            ),
+            ),)
           ],
         ),
       ),
